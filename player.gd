d0daa0871ff4dd7c79dc9b8d21f830a6f27e4f4d
Linyx <33364846+Linyx-Multi@users.noreply.github.com>
2020-06-17 15:10:36 +1200

    3

--- player.gd
+++ player.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
+#speed and HP export.
 export (int) var speed = 200
-
-var velocity = Vector2()
-var screen_size
 export (int) var max_health = 100
 
+#variables
+var velocity = Vector2()
+var screen_size
+
+#Get viewport size
 func _ready():
 	screen_size = get_viewport_rect().size
-	
+
+#Velocity and inputs
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
@@ -21,10 +25,12 @@ func get_input():
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
+#move and slide velocity stuff
 func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity)
 
+#animations
 func _process(delta):
 	if Input.is_action_pressed("down"):
 		$AnimatedSprite.play("walk_down")
